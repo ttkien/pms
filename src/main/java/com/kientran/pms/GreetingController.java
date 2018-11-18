e480dbc328754537a2ed6c350986fb612aaf9aa1
kientran <trankien@IB-028262-1Llocal.local>
2018-11-18 10:51:55 +0700

    init spring web

---
+++ src/main/java/com/kientran/pms/GreetingController.java
@@ -0,0 +1,12 @@
+package com.kientran.pms;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GreetingController {
+    @RequestMapping("/greeting")
+    public String greeting() {
+        return "greeting";
+    }
+}
