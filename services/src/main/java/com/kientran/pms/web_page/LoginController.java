1aa27abe3b0172172f78412fbbabe52d2dbc058b
Kien Tran <kien.tran@eastagile.com>
2018-11-19 20:59:21 +0700

    split inot 2 projects

---
+++ services/src/main/java/com/kientran/pms/web_page/LoginController.java
@@ -0,0 +1,102 @@
+package com.kientran.pms.web_page;
+
+import com.kientran.pms.authentication_service.request_models.AuthencationCredentials;
+import com.kientran.pms.web_page.security.SessionManager;
+import com.kientran.pms.web_page.security.UserResponse;
+import com.kientran.pms.web_page.services.AuthenticationServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Controller
+public class LoginController {
+    @Autowired
+    SessionManager sessionManager;
+
+    @Autowired
+    AuthenticationServiceInterface authenticationServiceInterface;
+
+    @GetMapping("/greeting1")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting.html";
+    }
+
+    @RequestMapping("/")
+    public void home(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            @RequestParam(name = "token", required = false) String token) {
+
+        if (token != null) {
+            sessionManager.setSessionWithToken(token);
+        }
+
+        if (sessionManager.isLogged()) {
+            navigateToPasswordManagement(response);
+        } else {
+            navigateToLoginPage(response);
+        }
+    }
+
+    private void navigateToLoginPage(HttpServletResponse response) {
+        try {
+            response.sendRedirect("login");
+        } catch (IOException e) {
+
+        }
+
+    }
+
+    private void navigateToPasswordManagement(HttpServletResponse response) {
+        try {
+            response.sendRedirect("/management");
+        } catch (IOException e) {
+
+        }
+    }
+
+    @RequestMapping(path = "/login", method = RequestMethod.GET)
+    public ModelAndView showLoginPage(HttpServletRequest request, HttpServletResponse response) {
+
+        Boolean isLogged = sessionManager.isLogged();
+        if (isLogged) {
+            navigateToPasswordManagement(response);
+            return null;
+        } else {
+            ModelAndView result = new ModelAndView("login");
+            result.addObject("test", "kienhipepsi");
+            return result;
+        }
+    }
+
+
+    @RequestMapping(path = "/login", method = RequestMethod.POST)
+    public void login(HttpServletRequest request,
+                        HttpServletResponse response,
+                        @RequestParam(name="username") String username,
+                        @RequestParam(name="passwordHash") String passwordHash
+                        ) {
+        UserResponse userResponse = null;
+        try {
+            userResponse = authenticationServiceInterface.authenticate(username, passwordHash).getBody();
+        } catch (Exception exception) {
+exception.printStackTrace();
+        }
+
+        if (userResponse == null) {
+            navigateToLoginPage(response);
+            return;
+        }
+
+        sessionManager.setSessionWithToken(userResponse);
+        navigateToPasswordManagement(response);
+    }
+}
